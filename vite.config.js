8efe64ddb071b3560403fbb99a03e0f7376248a5
Aditya Verma <138475873+AdityaExplorer@users.noreply.github.com>
2024-10-15 11:14:22 +0530

    'third_Commit'

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
+  base: '/port-react/',  // Set the base to match your GitHub repository name
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist',  // or 'build', depending on your folder structure
+  },
+});
