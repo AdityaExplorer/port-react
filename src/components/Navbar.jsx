89307ac0ad348d48d24d581b03cb58c3446271d0
Aditya Verma <138475873+AdityaExplorer@users.noreply.github.com>
2024-10-17 13:15:56 +0530

    'adjust_navbar_a_href'

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,6 @@
 import './Navbar.css';
 
 const Navbar=({selectedTab,setSelectedTab,onCertificateClick,onConatctClick })=>{
-
- 
-  
-
     return (
       <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom fixed-top">
       <div className="col-md-3 mb-2 mb-md-0">
